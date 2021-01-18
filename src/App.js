d52b979c6d426e0f028aae3a71d8d6f1a012bd41
Mehdi <mehdihannachi02@gmail.com>
2021-01-18 21:27:29 +0100

    Use class based component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Counter from "./Components/Counter/Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show: false,
+    };
+  }
+
+  setShow = () =>
+    this.setState({
+      show: !this.state.show,
+    });
+
+  render() {
+    return (
+      <div className="App">
+        <button className="visibility-btn" onClick={this.setShow}>
+          {this.state.show ? "Hide Counter" : "Show Counter"}
+        </button>
+
+        {/* { this.state.show && <Counter />} */}
+
+        {this.state.show ? <Counter /> : <h1>Hidden</h1>}
+      </div>
+    );
+  }
 }
 
 export default App;
