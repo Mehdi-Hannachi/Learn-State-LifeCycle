3571bbf8ecba3f9d4c7405f9b9f00ded31cf721f
Mehdi <mehdihannachi02@gmail.com>
2021-01-21 17:39:54 +0100

    update project with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Counter from "./Components/Counter/Counter";
 import Footer from "./Components/Footer/Footer";
@@ -6,48 +6,41 @@ import NavBar from "./Components/Navbar/NavBar";
 import { Button } from "react-bootstrap";
 import Pub from "./Components/Pub/Pub";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    let today = new Date(),
-      date =
-        today.getFullYear() +
-        "-" +
-        (today.getMonth() + 1) +
-        "-" +
-        today.getDate();
+const App = () => {
+  let today = new Date(),
+    mydate =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
 
-    this.state = {
-      show: false,
-      date: date,
-    };
-  }
+  const [show, setShow] = useState(false);
+  const [date, setDate] = useState(mydate);
 
-  setShow = () =>
-    this.setState({
-      show: !this.state.show,
-    });
+  return (
+    <div>
+      <NavBar date={date} />
+      <div className="mycounter">
+        <Button
+          variant="secondary"
+          size="lg"
+          onClick={() => setShow(!show)}
+          block
+        >
+          {show ? "Hide Counter" : "Show Counter"}
+        </Button>
 
-  render() {
-    return (
-      <div>
-        <NavBar date={this.state.date} />
-        <div className="mycounter">
-          <Button variant="secondary" size="lg" onClick={this.setShow} block>
-            {this.state.show ? "Hide Counter" : "Show Counter"}
-          </Button>
+        {/* { show && <Counter />} */}
 
-          {/* { this.state.show && <Counter />} */}
-
-          {this.state.show ? <Counter /> : <Pub />}
-        </div>
-
-        <div className="footer">
-          <Footer />
-        </div>
+        {show ? <Counter /> : <Pub />}
       </div>
-    );
-  }
-}
+
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,88 +1,140 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Badge } from "react-bootstrap";
 import "./counter.css";
 
-class Counter extends React.Component {
-  constructor() {
-    console.log("Contructor ()");
-    super();
-    this.state = {
-      counter: 0,
-      timer: 0,
-      intervall: null,
-    };
-  }
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const [timer, setTimer] = useState(0);
 
-  componentDidMount() {
-    console.log("Component Did Mount ()");
-    console.log(this.state.intervall);
-    this.setState({
-      intervall: setInterval(
-        () => this.setState({ timer: this.state.timer + 1 }),
-        1000
-      ),
-    });
-  }
-
-  componentDidUpdate() {
-    console.log("Component Did Update ()");
-    // console.log(this.state.intervall);
-  }
-
-  componentWillUnmount() {
-    console.log("Component Will Unmount ()");
-    clearInterval(this.state.intervall);
-  }
-
-  increment = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
+  const increment = () => {
+    setCounter(counter + 1);
   };
 
-  decrement = () => {
-    if (this.state.counter > 0) {
-      this.setState({
-        counter: this.state.counter - 1,
-      });
+  const decrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
     }
   };
 
-  reset = () => {
-    this.setState({
-      counter: 0,
-    });
-  };
+  // const chrono = () => {
 
-  render() {
-    console.log("render () ");
-    return (
-      <>
-        <div className="counter">
-          <Button variant="primary" onClick={this.decrement}>
-            -
-          </Button>
-          <span>{this.state.counter}</span>
+  // };
 
-          <Button variant="primary" onClick={this.increment}>
-            +
-          </Button>
+  useEffect(() => {
+    console.log("effect");
 
-          <Button variant="danger" onClick={this.reset}>
-            Reset
-          </Button>
-        </div>
+    const intervall = setInterval(() => {
+      setTimer(timer + 1);
+    }, 1000);
 
-        <Badge variant="secondary" className="timer">
-          Timer
-        </Badge>
+    return () => clearInterval(intervall);
+  }, [timer]);
 
-        <p>{`${Math.floor(this.state.timer / 3600)} : ${Math.floor(
+  // const reset = () => {
+  //   setCounter(0);
+  // };
+  return (
+    <>
+      <div className="counter">
+        <Button variant="primary" onClick={decrement}>
+          -
+        </Button>
+        <span>{counter}</span>
+
+        <Button variant="primary" onClick={increment}>
+          +
+        </Button>
+
+        <Button variant="danger" onClick={() => setCounter(0)}>
+          Reset
+        </Button>
+      </div>
+
+      <Badge variant="secondary" className="timer">
+        Timer
+      </Badge>
+
+      {/* {this.state.intervall !== null ? (
+        <button onClick={() => clearInterval(this.state.intervall) }>
+          Stop
+        </button>
+      ) : (
+        <button>Play</button>
+      )}
+      <button onClick={() => setInterval(1000)}>Play</button> */}
+
+      {/* onClick={() => {
+          this.state.intervall !== null
+            ? clearInterval(this.state.intervall)
+            : this.timer()
+        }} */}
+
+      {/* <button>{this.state.intervall !== null ? "arret" : "march"}</button> */}
+      <p>
+        {timer}
+        {/* {`${Math.floor(this.state.timer / 3600)} : ${Math.floor(
           (this.state.timer % 3600) / 60
-        )} : ${Math.floor((this.state.timer % 3600) % 60)}`}</p>
-      </>
-    );
-  }
-}
+        )} : ${Math.floor((this.state.timer % 3600) % 60)}`} */}
+      </p>
+    </>
+  );
+};
+
+// state = {
+//     counter: 0,
+//     timer: 0,
+//     intervall: null,
+//   };
+
+// timer = () => {
+//   this.setState({
+//     intervall: setInterval(
+//       () => this.setState({ timer: this.state.timer + 1 }),
+//       1000
+//     ),
+//   });
+// };
+
+// componentDidMount() {
+//   console.log("Component Did Mount ()");
+//   // console.log(this.state.intervall);
+//   // this.setState({
+//   //   intervall: setInterval(
+//   //     () => this.setState({ timer: this.state.timer + 1 }),
+//   //     1000
+//   //   ),
+//   // });
+//   this.timer();
+// }
+
+// componentDidUpdate() {
+//   console.log("Component Did Update ()");
+//   // console.log(this.state.intervall);
+// }
+
+// componentWillUnmount() {
+//   console.log("Component Will Unmount ()");
+//   // clearInterval(this.state.intervall);
+// }
+
+// increment = () => {
+//   this.setState({
+//     counter: this.state.counter + 1,
+//   });
+// };
+
+// decrement = () => {
+//   if (this.state.counter > 0) {
+//     this.setState({
+//       counter: this.state.counter - 1,
+//     });
+//   }
+// };
+
+// reset = () => {
+//   this.setState({
+//     counter: 0,
+//   });
+// };
 
 export default Counter;
